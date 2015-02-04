2c1e82cd1b8098b9a4191063ca54f6fd3e875251
Kavin Yao <kavinyao@gmail.com>
2015-02-04 14:24:28 -0800

    Add new interfaces and example usage

--- liu.py
+++ liu.py
@@ -1,13 +1,21 @@
 from abc import ABCMeta, abstractmethod
+from collections import defaultdict
 from functools import reduce
 
 class ControlFlowGraph:
     """It is the user's duty to ensure all nodes must be reachable from entry."""
 
-    class BasicBlock:
-        def __init__(self, cfg, no):
+    class QuadBlock:
+        """A simple block with only 1 statement."""
+        def __init__(self, cfg, no, operation=Noop):
             self.cfg = cfg
             self.no = no
+            self.op = operation
+
+            # record IN and OUT for different analyses
+            # the key is name of an analysis
+            self.IN = {}
+            self.OUT = {}
 
         def is_entry(self):
             return self == self.cfg.entry
@@ -21,11 +29,21 @@ class ControlFlowGraph:
         def successors(self):
             return self.cfg.succ[self]
 
+        def defined_variables(self):
+            return self.op.defined_variables()
+
+        def used_variables(self):
+            return self.op.used_variables()
+
         def __eq__(self, other):
             return self.cfg == other.cfg and self.no == other.no
 
         def __hash__(self):
             return hash(self.no)
+
+
+    # for now, let's use QuadBlock as BasicBlock
+    BasicBlock = QuadBlock
 
 
     def __init__(self):
@@ -43,8 +61,8 @@ class ControlFlowGraph:
     def exit(self):
         return self.exit
 
-    def new_block(self):
-        block = BasicBlock(self, self.counter)
+    def new_block(self, operation=Noop):
+        block = BasicBlock(self, self.counter, operation)
 
         # bookkeeping
         self.pred[block] = set()
@@ -54,8 +72,16 @@ class ControlFlowGraph:
         return block
 
     def add_edge(self, src, dst):
+        assert dst not in self.succ[src]
+
         self.succ[src].add(dst)
         self.pred[dst].add(src)
+
+    def remove_edge(self, src, dst):
+        assert dst in self.succ[src]
+
+        self.succ[src].remove(dst)
+        self.pred[dst].remove(src)
 
     def blocks(self):
         """Return an iterator of all blocks."""
@@ -80,6 +106,19 @@ class ControlFlowGraph:
 
 class FlowAnalysis:
     __metaclass__ = ABCMeta
+    analyses = set()
+
+    def __init__(self):
+        # make sure the new analysis is unique
+        # since we are going to use the name as key
+        name = self.name()
+        assert name not in FlowAnalysis.analyses
+        FlowAnalysis.analyses.add(name)
+
+    @abstractmethod
+    def name(self):
+        """Return a unique name of the analysis."""
+        pass
 
     @abstractmethod
     def is_forward(self):
@@ -127,15 +166,16 @@ class Solver:
 
     @staticmethod
     def __run_forward(analysis, cfg):
+        A = analysis.name()
+
         analysis.preprocess(cfg)
 
-        # initialize OUT
-        OUT = {}
+        # initialize OUT of each block
         for block in cfg.blocks():
             if block.is_entry():
-                OUT[block] = analysis.boundary_value()
+                block.OUT[A] = analysis.boundary_value()
             else:
-                OUT[block] = analysis.initial_value()
+                block.OUT[A] = analysis.initial_value()
 
         # iterate until convergence
         changed = True
@@ -145,11 +185,92 @@ class Solver:
             for block in cfg.iterate(post_order=False):
                 if block.is_entry(): continue
 
-                IN = reduce(analysis.meet, OUT[p] for p in block.predecessors())
-                old_OUT = OUT[B]
-                OUT[block] = analysis.transfer(block, IN)
+                block.IN[A] = reduce(analysis.meet, p.OUT[A] for p in block.predecessors())
+                old_OUT = B.OUT[A]
+                block.OUT = analysis.transfer(block, block.IN[A])
 
-                if old_OUT != OUT[block]:
+                if old_OUT != block.OUT[A]:
                     changed = True
 
         analysis.postprocess(cfg)
+
+
+# example analyses
+class ReachingDefinition(FlowAnalysis):
+
+    def __init__(self):
+        super().__init__()
+
+    def name(self):
+        return 'Reaching-Definition'
+
+    def is_forward(self):
+        return True
+
+    def boundary_value(self):
+        return set()
+
+    def initial_value(self):
+        return set()
+
+    def meet(self, val1, val2):
+        """Meet is union of two sets."""
+        return val1 | val2
+
+    def transfer(self, block, val):
+        """Apply transfer function on block. Must NOT alter val."""
+        result = val
+
+        defined_vars = block.defined_vars()
+        # process only if this block defines some variable
+        if len(defined_vars) > 0:
+            for var in defined_vars:
+                result -= self.defs[var]
+
+            result.add(block)
+
+        return result
+
+    def preprocess(self, cfg):
+        """Gather definitions for each variable."""
+        defs = defaultdict(set) # var -> set[block]
+        for block in cfg.blocks():
+            for var in block.defined_variables():
+                defs[var].add(block)
+
+        self.defs = defs
+
+    def postprocess(self, cfg):
+        """Do postprocessing (optional)."""
+        pass
+
+# example usage
+if __name__ == '__main__':
+    # construct CFG
+    cfg = ControlFlowGraph()
+    n2 = cfg.new_block(Assign(Var('i'), Subtract(Var('m'), Const('1'))))
+    n3 = cfg.new_block(Assign(Var('j'), Var('n')))
+    n4 = cfg.new_block(Assign(Var('a'), Var('u1')))
+    n5 = cfg.new_block(Assign(Var('i'), Add(Var('i'), Const('1'))))
+    n6 = cfg.new_block(Assign(Var('j'), Subtract(Var('j'), Const('1'))))
+    n7 = cfg.new_block(Assign(Var('a'), Var('u2')))
+    n8 = cfg.new_block(Assign(Var('u'), Var('u3')))
+    cfg.add_edge(cfg.entry(), n2)
+    cfg.add_edge(n2, n3)
+    cfg.add_edge(n3, n4)
+    cfg.add_edge(n4, n5)
+    cfg.add_edge(n5, n6)
+    cfg.add_edge(n6, n7)
+    cfg.add_edge(n6, n8)
+    cfg.add_edge(n7, n8)
+    cfg.add_edge(n8, n5)
+    cfg.add_edge(n8, cfg.exit())
+
+    # solve reaching definitions
+    reach_def = ReachingDefinition()
+    Solver.solve(reach_def, cfg)
+
+    RD = reach_def.name()
+    for block in cfg.iterate(post_order=False):
+        print('B{}.in = {}'.format(block.no, block.IN[RD]))
+        print('B{}.out = {}'.format(block.no, block.OUT[RD]))
